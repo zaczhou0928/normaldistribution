2ab9fb8bfa42fda440306685d7b0ce2f172f6057
Meow <qianjin@Qianjins-MBP.attlocal.net>
2024-06-02 15:36:54 -0700

    test

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   import Meta from "./Meta.svelte";
   import Title from "./Components/Title.svelte";
   import Intro from "./Components/Intro.svelte";
-  import TextAndMathEquations from "./Components/TextAndMathEquations.svelte";
+  import Quiz from "./Components/Quiz.svelte";
   import Conclusion from "./Components/Conclusion.svelte";
   import ScrollCenter from "./Components/ScrollCenter.svelte";
   import Resources from "./Components/Resources.svelte";
@@ -13,8 +13,7 @@
 <Meta />
 <Title />
 <Intro />
-<Histogram />
 <ScrollCenter />
 <ScrollSide />
-<TextAndMathEquations />
+<Quiz />
 <Conclusion />
